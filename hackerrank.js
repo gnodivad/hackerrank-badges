688535785e76331cd00d020bb4aef8569659e987
David <dodwei.dev@gmail.com>
2019-03-16 17:05:35 +0800

    Take sceenshot on a individual badge and save to local filesystem

--- hackerrank.js
+++ hackerrank.js
@@ -8,11 +8,21 @@ async function run() {
 
     await page.goto("https://www.hackerrank.com/DavidODW");
 
-    let badgesListLength = await page.evaluate(sel => {
-        let element = document.querySelector(sel);
+    await page.waitForSelector("div > div.badges-list > div:nth-child(1) > div > div > svg");
 
-        return element.childElementCount;
-    }, BADGE_LIST_SELECTOR);
+    const svgImage = await page.$("div > div.badges-list > div:nth-child(1) > div > div > svg");
+
+    await svgImage.screenshot({
+        path: "screenshots/logo-screenshot.jpg",
+        type: "jpeg",
+        quality: 100
+    });
+
+    // let badgesListLength = await page.evaluate(sel => {
+    //     let element = document.querySelector(sel);
+
+    //     return element.childElementCount;
+    // }, BADGE_LIST_SELECTOR);
 
     browser.close();
 }
